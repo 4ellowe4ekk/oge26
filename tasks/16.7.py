db457912f81cafdd2ab75b66653f20d85681ee4d
andrey <4ellowe4ek999@gmail.com>
2026-05-07 18:36:02 +0300

    Седьмая прога!

---
+++ tasks/16.7.py
@@ -0,0 +1,11 @@
+n = int(input("Введите колво чисел: "))
+m = 0
+
+for i in range(n):
+	chislo = int(input("Введите число: "))
+
+	if n <= 1000 and chislo <= 30000 and chislo % 10 == 3:
+		if chislo > m:
+			m = chislo
+
+print(m)
