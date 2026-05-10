3e335acc763c39dc5e02f0a1841117f062cd489f
4ellowe4ekk <4ellowe4ek999@gmail.com>
2026-05-10 15:42:37 +0300

    Двадцатая задача!

---
+++ tasks/16.20.py
@@ -0,0 +1,14 @@
+s = 0
+
+while True:
+    chislo = int(input("Введите число: "))
+    
+    if chislo != 0:
+        if chislo % 8 == 0:
+            if chislo % 10 == 6:
+                s += chislo
+        
+    else:
+        break
+
+print(s)
