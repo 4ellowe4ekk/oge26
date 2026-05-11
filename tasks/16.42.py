c99447ad5d58bb6bfa30b02b3accff9008577847
4ellowe4ekk <4ellowe4ek999@gmail.com>
2026-05-11 12:21:02 +0300

    Сорок вторая задача!

---
+++ tasks/16.42.py
@@ -0,0 +1,16 @@
+n = int(input("Введите колво чисел: "))
+kolvo = 0
+s = 0
+
+for i in range(n):
+    chislo = int(input("Введите число: "))
+
+    if chislo % 7 % 10 == 5:
+        kolvo += 1
+        s += chislo
+
+if kolvo > 0:
+    print(s / kolvo)
+
+else:
+    print('NO')
